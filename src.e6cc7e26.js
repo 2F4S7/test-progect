7971f7c52a789aa8b5b9c83041f9a902a06b64b4
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-05-07 13:30:36 +0000

    Deploying to gh-pages from @ Oksana-Mazur58/test-progect@b7b24bd5af7697250a82da6cce1922c95e9d7066 🚀

--- src.e6cc7e26.js
+++ src.e6cc7e26.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-progect/src.361f78d8.js.map
+//# sourceMappingURL=/test-progect/src.e6cc7e26.js.map
